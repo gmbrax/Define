f32bfd8124bfc1f0be3e165cd4939fd07ffee1c3
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2025-11-03 19:37:38 -0300

    Add `check_configure_mode` and `configure` methods to `UI` class for handling configuration mode during execution.

--- src/define/ui/ui.py
+++ src/define/ui/ui.py
@@ -6,12 +6,23 @@ class UI:
         self.__parent = parent
         self.console = Console()
 
+    def check_configure_mode(self):
+        if self.__parent.run_mode is False:
+            self.console.print("[bold red]Running on Configuration Mode:[/bold red]")
+            return True
+        return False
+
+    def configure(self):
+        exit(0)
+
     def check_configuration(self):
         if not self.__parent.is_configured:
             self.console.print("[bold red]Please configure before running this program.[/bold red]")
             exit(1)
 
     def run(self):
+        if self.check_configure_mode():
+            self.configure()
 
         self.console.print("[bold]Define[/bold] [italic white]V.0.0.1[/italic white ] by Gustavo Henrique S. S. de Miranda\n")
         self.console.print("Definitions and thesaurus data provided by Merriam-Webster, Inc. © Merriam-Webster, Inc. All rights reserved.")
